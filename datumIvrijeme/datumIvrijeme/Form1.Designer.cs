203b30cd179b6d5ba2ee4ecb44bceebd3c7bb56a
LeonCicak <leoncicak07@gmail.com>
2024-12-16 13:23:35 +0100

    Add files via upload

---
+++ datumIvrijeme/datumIvrijeme/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace datumIvrijeme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePickerDatum = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerVrijeme = new System.Windows.Forms.DateTimePicker();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePickerDatum.Location = new System.Drawing.Point(283, 31);
+            this.dateTimePickerDatum.Name = "dateTimePicker1";
+            this.dateTimePickerDatum.Size = new System.Drawing.Size(395, 26);
+            this.dateTimePickerDatum.TabIndex = 0;
+            this.dateTimePickerDatum.Value = new System.DateTime(2024, 12, 16, 12, 47, 45, 0);
+            this.dateTimePickerDatum.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePickerVrijeme.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePickerVrijeme.Location = new System.Drawing.Point(283, 114);
+            this.dateTimePickerVrijeme.Name = "dateTimePicker2";
+            this.dateTimePickerVrijeme.Size = new System.Drawing.Size(395, 26);
+            this.dateTimePickerVrijeme.TabIndex = 2;
+            this.dateTimePickerVrijeme.Value = new System.DateTime(2024, 12, 16, 0, 0, 0, 0);
+            this.dateTimePickerVrijeme.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(49, 212);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(629, 182);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(283, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Rezervacija";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Upisi datum polaska";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Upisi vrijeme polaska";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.dateTimePickerVrijeme);
+            this.Controls.Add(this.dateTimePickerDatum);
+            this.Name = "Form1";
+            this.Text = "Upisi vrijeme polaska";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePickerDatum;
+        private System.Windows.Forms.DateTimePicker dateTimePickerVrijeme;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
